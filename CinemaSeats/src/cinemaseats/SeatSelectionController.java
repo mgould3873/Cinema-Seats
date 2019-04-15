abf08eb38903ed2d76718691a286bbf483bcfe42
Matthew <mgould3873@floridapoly.edu>
2019-04-15 09:44:34 -0400

    Added the 'reserve seats' button FXID

--- CinemaSeats/src/cinemaseats/SeatSelectionController.java
+++ CinemaSeats/src/cinemaseats/SeatSelectionController.java
@@ -17,8 +17,12 @@ public class SeatSelectionController {
     private Button backToMovieListButtion;
 
     @FXML
+    private Button reserveSeatsButton;
+
+    @FXML
     void initialize() {
         assert backToMovieListButtion != null : "fx:id=\"backToMovieListButtion\" was not injected: check your FXML file 'seatSelection.fxml'.";
+        assert reserveSeatsButton != null : "fx:id=\"reserveSeatsButton\" was not injected: check your FXML file 'seatSelection.fxml'.";
 
     }
 }
