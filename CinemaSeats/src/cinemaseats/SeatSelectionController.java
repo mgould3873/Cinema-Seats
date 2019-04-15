5fb39029c81306a437bad7282180201d718c109a
Matthew <mgould3873@floridapoly.edu>
2019-04-15 09:42:26 -0400

    Added the seat selection screen, thus finishing all the screens. Now we just have to tie them together with the buttons.

---
+++ CinemaSeats/src/cinemaseats/SeatSelectionController.java
@@ -0,0 +1,24 @@
+package cinemaseats;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SeatSelectionController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button backToMovieListButtion;
+
+    @FXML
+    void initialize() {
+        assert backToMovieListButtion != null : "fx:id=\"backToMovieListButtion\" was not injected: check your FXML file 'seatSelection.fxml'.";
+
+    }
+}
