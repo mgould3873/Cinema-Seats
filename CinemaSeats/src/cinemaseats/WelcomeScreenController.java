1a0f8db48e0fb6b86afbeae2fa10c4f2e821768c
Matthew <mgould3873@floridapoly.edu>
2019-04-16 13:07:52 -0400

    Added options to choiceBox as well as fixed WelcomeScreen.fxml and controller not linking properly.

--- CinemaSeats/src/cinemaseats/WelcomeScreenController.java
+++ CinemaSeats/src/cinemaseats/WelcomeScreenController.java
@@ -2,6 +2,7 @@ package cinemaseats;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -16,20 +17,21 @@ public class WelcomeScreenController {
     private URL location;
 
     @FXML
-    private ChoiceBox<?> theaterChoiceBox;
+    private ChoiceBox theaterChoiceBox;
 
     @FXML
     private Button toMovieSelectionsButton;
 
     @FXML
     void toMovieSelections(ActionEvent event) {
-
+        System.out.println("toMovieSelectionsButton Clicked");
     }
 
     @FXML
     void initialize() {
-        assert theaterChoiceBox != null : "fx:id=\"theaterChoiceBox\" was not injected: check your FXML file 'MovieList.fxml'.";
-        assert toMovieSelectionsButton != null : "fx:id=\"toMovieSelectionsButton\" was not injected: check your FXML file 'MovieList.fxml'.";
-
+        assert theaterChoiceBox != null : "fx:id=\"theaterChoiceBox\" was not injected: check your FXML file 'WelcomeScreen.fxml'.";
+        assert toMovieSelectionsButton != null : "fx:id=\"toMovieSelectionsButton\" was not injected: check your FXML file 'WelcomeScreen.fxml'.";
+        
+        theaterChoiceBox.setItems(FXCollections.observableArrayList("Regal", "Cinemark", "AMC"));
     }
 }
