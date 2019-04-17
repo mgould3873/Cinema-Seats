351e6d6b9e997b6363610da8025ea228b0aefaf2
Matthew <mgould3873@floridapoly.edu>
2019-04-17 09:15:12 -0400

    Added functions to buttons in order to switch screens.

--- CinemaSeats/src/cinemaseats/WelcomeScreenController.java
+++ CinemaSeats/src/cinemaseats/WelcomeScreenController.java
@@ -1,12 +1,17 @@
 package cinemaseats;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class WelcomeScreenController {
 
@@ -15,6 +20,9 @@ public class WelcomeScreenController {
 
     @FXML
     private URL location;
+    
+    @FXML
+    private AnchorPane welcomeScreenAnchorPane;
 
     @FXML
     private ChoiceBox theaterChoiceBox;
@@ -24,14 +32,31 @@ public class WelcomeScreenController {
 
     @FXML
     void toMovieSelections(ActionEvent event) {
-        System.out.println("toMovieSelectionsButton Clicked");
+        //Switch to movie list screen
+        Parent root = null;
+        FXMLLoader root1 = null;
+        root1 = new FXMLLoader(getClass().getResource("MovieList.fxml"));
+        try {
+            root = (Parent) root1.load();
+        } catch (IOException ex) {
+        }
+        MovieListController controller = root1.getController();
+        Scene sc = new Scene(root);
+        Stage st = new Stage();
+        st.setScene(sc);
+        st.show();
+        welcomeScreenAnchorPane.getScene().getWindow().hide();
     }
 
     @FXML
     void initialize() {
+        assert welcomeScreenAnchorPane != null : "fx:id=\"welcomeScreenAnchorPane\" was not injected: check your FXML file 'WelcomeScreen.fxml'.";
         assert theaterChoiceBox != null : "fx:id=\"theaterChoiceBox\" was not injected: check your FXML file 'WelcomeScreen.fxml'.";
         assert toMovieSelectionsButton != null : "fx:id=\"toMovieSelectionsButton\" was not injected: check your FXML file 'WelcomeScreen.fxml'.";
         
-        theaterChoiceBox.setItems(FXCollections.observableArrayList("Regal", "Cinemark", "AMC"));
+        //Adding choices to the choiceBox menu
+        theaterChoiceBox.getItems().add("Regal Cinema");
+        theaterChoiceBox.getItems().add("AMC Theatre");
+        theaterChoiceBox.getItems().add("Cinepolis");
     }
 }
